232741048b8d2db797f59a528e169df5ee922897
Andrei Buiu <andrei@buiu.co.uk>
2023-09-29 20:18:59 +0300

    added frontend

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const Sentiment = require('sentiment');
 const winston = require('winston');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 1337;
 
 app.use(express.json());
 
